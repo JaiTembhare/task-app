b83d5f7adb173bc28773f9e3b93ec220f789180e
Jai Tembhare <jaitembhare@gmail.com>
2023-12-12 07:00:54 +0530

    MongoDB Connected

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-const server = require("express");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const app = express();
+// creating Routes
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {}
+};
+startServer();
